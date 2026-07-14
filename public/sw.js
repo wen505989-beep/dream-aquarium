aa835be5e05c5855a9508e697353ca8d76a3283b
wen505989-beep <wen505989-beep@users.noreply.github.com>
2026-07-14 23:39:34 +0800

    Always refresh the latest aquarium page

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,18 @@
-const CACHE = 'dream-aquarium-v12';
+const CACHE = 'dream-aquarium-v13';
 const BASE_URL = new URL('./', self.registration.scope).pathname;
 const ASSETS = ['', 'index.html', 'manifest.webmanifest', 'icon.svg'].map(asset => `${BASE_URL}${asset}`);
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).then(response => {
+      const clone = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, clone));
+      return response;
+    }).catch(() => caches.match(event.request).then(hit => hit || caches.match(`${BASE_URL}index.html`))));
+    return;
+  }
   event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
     const clone = response.clone();
     if (new URL(event.request.url).origin === location.origin) caches.open(CACHE).then(cache => cache.put(event.request, clone));
